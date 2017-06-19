03b23cbfa51b82fb5b973419e6619205a0a921b2
Abi Evans <abievans@uw.edu>
2017-06-19 10:30:14 -0700

    Removed test url

--- coursedashboards/urls.py
+++ coursedashboards/urls.py
@@ -1,17 +1,9 @@
 from django.conf.urls import include, url
 from django.views.generic.base import RedirectView
-from coursedashboards.views import (
-    HomeView,
-    CoursePageView
-)
 from coursedashboards.views.index import index
 
 
 urlpatterns = [
     # Home
-    url(r'^course-page$', CoursePageView.as_view(), name='coursepage'),
-    url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^$', RedirectView.as_view(url='/course-page')),
-    #FOR TESTING DATA, WILL REMOVE
-    url(r'^test$', index, name="login_test")
+    url(r'^$', index, name='home')
 ]
