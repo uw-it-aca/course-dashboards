1b29c9f6120381b07556e9b4762e18b2e8f0d10c
Abi Evans <abievans@uw.edu>
2017-07-12 12:17:55 -0700

    Fixed misbehaving historic data selectors

--- coursedashboards/static/coursedashboards/js/main.js
+++ coursedashboards/static/coursedashboards/js/main.js
@@ -10,16 +10,17 @@ $("#top_banner").html(template({
 
 //Listed for course dropdown selection change
 $("#my_courses").change(function() {
-    console.log("change");
     var index = $("select[name='my_courses'] option:selected").index();
     showCurrentCourseData(index);
+    showHistoricDataSelectors(index);
     showHistoricCourseData(index, "All Quarters", "All Years");
 });
 
 
 var index = $("select[name='my_courses'] option:selected").index();
 showCurrentCourseData(index);
-showHistoricCourseData(index);
+showHistoricDataSelectors(index, "All Quarters", "All Years");
+showHistoricCourseData(index, "All Quarters", "All Years");
 
 //Display data about the currently selected course - called whenever selection changes
 function showCurrentCourseData(index) {
@@ -35,6 +36,79 @@ function showCurrentCourseData(index) {
     }));
 }
 
+//Populate the historic data selectors for the currently selected course
+function showHistoricDataSelectors(index, quarter, year) {
+    //Fix case of past quarters and create arrays without duplicate quarters/years
+    var valid_quarters = [];
+    var valid_years = [];
+    var combined = [];//stores quarter + year so that when a quarter is selected, can only select years where the course was offered in that quarter & vice versa
+    var past_offerings = window.section_data[index].past_offerings;
+    for (var o in past_offerings) {
+        past_offerings[o].quarter = firstLetterUppercase(past_offerings[o].quarter);
+        if (combined.indexOf(past_offerings[o].quarter + past_offerings[o].year) == -1)
+            combined.push(past_offerings[o].quarter + past_offerings[o].year);
+        if (valid_quarters.indexOf(past_offerings[o].quarter) == -1)
+            valid_quarters.push(past_offerings[o].quarter);
+        if (valid_years.indexOf(past_offerings[o].year) == -1)
+            valid_years.push(past_offerings[o].year);
+    }
+    //Remove any invalid combinations of quarter and year based on current selection
+    if (quarter != "All Quarters") {
+        var remove_years = [];
+        for (var y in valid_years)
+            if (combined.indexOf(quarter + valid_years[y]) == -1)
+                remove_years.push(valid_years[y]);
+        while (remove_years.length > 0) {
+            valid_years.splice(valid_years.indexOf(remove_years[0]),1);
+            remove_years.splice(0,1);
+        }
+    }
+    if (year != "All Years") {
+        var remove_quarters = [];
+        for (var q in valid_quarters)
+            if (combined.indexOf(valid_quarters[q] + year) == -1)
+                remove_quarters.push(valid_quarters[q]);
+        while (remove_quarters.length > 0) {
+            valid_quarters.splice(valid_quarters.indexOf(remove_quarters[0]),1);
+            remove_quarters.splice(0,1);
+        }
+    }
+    
+    //Load template
+    var selectors = $("#historic-data-selectors").html();
+    var selectorsTemplate = Handlebars.compile(selectors);
+    $("#historic-selector-target").html(selectorsTemplate({
+        past_quarters:prepOptions(quarter, valid_quarters, "All Quarters"), past_years:prepOptions(year, valid_years, "All Years")
+    }));
+    
+    //Historic data selection
+    $(".historic-filter").change(function() {
+        console.log("selection change ");
+        index = $("select[name='my_courses'] option:selected").index();
+        var newQuarter = $("select[name='historic_filter_quarter'] option:selected").val();
+        var newYear = $("select[name='historic_filter_year'] option:selected").val();
+        showHistoricDataSelectors(index, newQuarter, newYear);
+        showHistoricCourseData(index, newQuarter, newYear);
+    });
+}
+
+//Prepares the handlebars variable for historic data selectors
+function prepOptions(selected, valid, all) {
+    options = [];
+    //make sure All option gets pushed
+    options.push({
+        name: all,
+        selected: (selected==all)? "selected":""
+    });
+    for (var v in valid) {
+        options.push({
+            name: valid[v],
+            selected: (valid[v]==selected) ? "selected":""
+        });
+    }
+    return options;
+}
+
 //Display data about the past offerings of selected course - called whenever selection changes
 function showHistoricCourseData(index, quarter, year) {
     var past_offerings = window.section_data[index].past_offerings;
@@ -42,113 +116,84 @@ function showHistoricCourseData(index, quarter, year) {
         past_offerings[i].quarter = firstLetterUppercase(past_offerings[i].quarter);
     var historic = $("#historic-course-data").html();
     var historicTemplate = Handlebars.compile(historic);
-    var quarter = $("select[name='historic_filter_quarter'] option:selected").val();
-    if (quarter == undefined)
-        quarter = "All Quarters";
-    var year = $("select[name='historic_filter_year'] option:selected").val();
-    if (year == undefined)
-        year = "All Years";
+    console.log("show historic data for " + quarter + " " + year);
     $("#historic-course-target").html(historicTemplate({
-        past_terms:window.section_data[index].past_offerings,
-        common_majors:calculateCommonMajorsForOffering(index, quarter, year),
-        latest_majors:calculateRecentMajorsForOffering(index, quarter, year),
-        common_courses:commonConcurrentCoursesForOffering(index, quarter, year),
+        common_majors:calculateCommon(index, quarter, year, "majors","major"),
+        latest_majors:calculateCommon(index, quarter, year, "latest_majors","major"),
+        common_courses:calculateCommon(index, quarter, year, "concurrent_courses","course"),
         selected_quarter:quarter,
-        selected_year:year
+        selected_year:year,
+        instructors: getInstructors(index, quarter, year)
+        //past_terms:window.section_data[index].past_offerings
     }));
 }
 
-//Historic quarter selection
-$("#historic_filter_quarter").change(function() {
-    console.log("quarter change");
-    var index = $("select[name='my_courses'] option:selected").index();
-    showCurrentCourseData(index);
-    showHistoricCourseData(index, $("select[name='historic_filter_quarter'] option:selected").val(), $("select[name='historic_filter_year'] option:selected").val());
-});
-
-$("#historic_filter_year").change(function() {
-    console.log("year change");
-    var index = $("select[name='my_courses'] option:selected").index();
-    showCurrentCourseData(index);
-    showHistoricCourseData(index, $("select[name='historic_filter_quarter'] option:selected").val(), $("select[name='historic_filter_year'] option:selected").val());
-})
-
-//On page load, quarter and year are undefined
-//When dropdown changed, page redraw shows default selection
-function calculateCommonMajorsForOffering(index, quarter, year) {
+function getInstructors(index, quarter, year) {
     var all_quarters = "All Quarters";
     var all_years = "All Years";
     var past_offerings = window.section_data[index].past_offerings;
-    var majors = {};
-    var total_students = 0;
-    if (quarter != all_quarters && year != all_years) {
-        for (var o = 0; o < past_offerings.length; o++) {
-            if (past_offerings[o].quarter == quarter && past_offerings[o].year == year)
-                majors = past_offerings[o].majors;
-        }
-    }
-    else {
-        for (var o = 0; o < past_offerings.length; o++) {
-            if (quarterIsInRange(past_offerings[o], year, all_years, quarter, all_quarters)) {
-                var term_majors = past_offerings[o].majors;
-                for (var m = 0; m < term_majors.length; m++) {
-                    total_students += term_majors[m].number_students;
-                    if (majors.hasOwnProperty(term_majors[m].major))
-                        majors[term_majors[m].major] += term_majors[m].number_students;
-                    else majors[term_majors[m].major] = term_majors[m].number_students;
-                }
+    var instructors = []
+    for (var o = 0; o < past_offerings.length; o++) {
+        if (quarterIsInRange(past_offerings[o], year, all_years, quarter, all_quarters)) {
+            for (var i in past_offerings[o].instructors) {
+                instructors.push({
+                    quarter: past_offerings[o].quarter,
+                    year: past_offerings[o].year,
+                    display_name: past_offerings[o].instructors[i].display_name,
+                    uw_email: past_offerings[o].instructors[i].uwnetid + "@uw.edu"
+                });
             }
         }
-        
     }
-    return sortMajors(majors, total_students);
+    return instructors;
 }
 
-function calculateRecentMajorsForOffering(index, quarter, year) {
+
+//Calculates all of the common major/course lists based on historic selections
+function calculateCommon(index, quarter, year, list_type, name_type) {
     var all_quarters = "All Quarters";
     var all_years = "All Years";
     var past_offerings = window.section_data[index].past_offerings;
-    var majors = {};
+    var obj = {};
     var total_students = 0;
-    if (quarter != all_quarters && year != all_years) {
-        for (var o = 0; o < past_offerings.length; o++) {
-            if (past_offerings[o].quarter == quarter && past_offerings[o].year == year)
-                majors = past_offerings[o].latest_majors;
-        }
-    }
-    else {
-        for (var o = 0; o < past_offerings.length; o++) {
-            if (quarterIsInRange(past_offerings[o], year, all_years, quarter, all_quarters)) {
-                var term_majors = past_offerings[o].latest_majors;
-                for (var m = 0; m < term_majors.length; m++) {
-                    total_students += term_majors[m].number_students;
-                    if (majors.hasOwnProperty(term_majors[m].major))
-                        majors[term_majors[m].major] += term_majors[m].number_students;
-                    else majors[term_majors[m].major] = term_majors[m].number_students;
-                }
+    for (var o = 0; o < past_offerings.length; o++) {
+        if (quarterIsInRange(past_offerings[o], year, all_years, quarter, all_quarters)) {
+            var term_obj = past_offerings[o][list_type];
+            for (var m = 0; m < term_obj.length; m++) {
+                total_students += term_obj[m].number_students;
+                if (obj.hasOwnProperty(term_obj[m][name_type]))
+                    obj[term_obj[m][name_type]] += term_obj[m].number_students;
+                else obj[term_obj[m][name_type]] = term_obj[m].number_students;
             }
         }
-        
     }
-    return sortMajors(majors, total_students);
+    return sortObj(obj, total_students, name_type);
 }
 
+//check if past offering was in the range selected in the dropdowns
 function quarterIsInRange(past_offering, year, all_years, quarter, all_quarters) {
     if ((past_offering.year == year && quarter == all_quarters) ||
         (year == all_years && past_offering.quarter == quarter) ||
-        (year == all_years && quarter == all_quarters))
+        (year == all_years && quarter == all_quarters) ||
+        (year != all_years && quarter != all_quarters)
+       )
         return true;
     else return false;
 }
 
-function sortMajors(majors, total_students) {
+//order majors/courses by number of students
+function sortObj(arr, total_students, name_type) {
     var sorted = [];
-    for (var m in majors) {
-        sorted.push({"major":m, "number_students":majors[m], "percent_students": ((majors[m]/total_students)*100).toFixed(2)});
+    for (var i in arr) {
+        if (name_type == "major")
+            sorted.push({"major":i, "number_students":arr[i], "percent_students": ((arr[i]/total_students)*100).toFixed(2)});
+        else
+            sorted.push({"course":i, "number_students":arr[i], "percent_students": ((arr[i]/total_students)*100).toFixed(2)});
     }
     return reverseSortByNumStudents(sorted);
 }
 
+//sort array of objects by number_students value
 function reverseSortByNumStudents(arr) {
     arr.sort(function(a, b) {
         return a.number_students - b.number_students;
@@ -156,38 +201,6 @@ function reverseSortByNumStudents(arr) {
     return arr.reverse();
 }
 
-function commonConcurrentCoursesForOffering(index, quarter, year) {
-    var all_quarters = "All Quarters";
-    var all_years = "All Years";
-    var past_offerings = window.section_data[index].past_offerings;
-    var courses = {};
-    var total_students = 0;
-    if (quarter != all_quarters && year != all_years) {
-        for (var o = 0; o < past_offerings.length; o++) {
-            if (past_offerings[o].quarter == quarter && past_offerings[o].year == year)
-                courses = past_offerings[o].concurrent_courses;
-        }
-    }
-    else {
-        for (var o = 0; o < past_offerings.length; o++) {
-            if (quarterIsInRange(past_offerings[o], year, all_years, quarter, all_quarters)) {
-                var term_courses = past_offerings[o].concurrent_courses;
-                for (var m = 0; m < term_courses.length; m++) {
-                    total_students += term_courses[m].number_students;
-                    if (courses.hasOwnProperty(term_courses[m].major))
-                        courses[term_courses[m].course] += term_courses[m].number_students;
-                    else courses[term_courses[m].course] = term_courses[m].number_students;
-                }
-            }
-        }
-        
-    }
-    var sorted = [];
-    for (var m in courses) {
-        sorted.push({"course":m, "number_students":courses[m], "percent_students": ((courses[m]/total_students)*100).toFixed(2)});
-    }
-    return reverseSortByNumStudents(sorted);
-}
 
 //Capitalize the first letter of a word
 function firstLetterUppercase(word) 
