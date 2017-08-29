1435ddd06843ff98257fb4dedcf1b96281fdbba4
mike seibel <mikes@washington.edu>
2017-08-29 16:15:56 -0700

    adds course offering to capture term-specific data

--- coursedashboards/models/course.py
+++ coursedashboards/models/course.py
@@ -6,6 +6,17 @@ class Course(models.Model):
     curriculum = models.CharField(max_length=6)
     course_number = models.PositiveSmallIntegerField()
     section_id = models.CharField(max_length=2)
+
+
+class CourseOffering(models.Model):
+    term = models.ForeignKey(Term,
+                             on_delete=models.PROTECT)
+    course = models.ForeignKey(Course,
+                               on_delete=models.PROTECT)
     current_enrollment = models.PositiveSmallIntegerField()
     limit_estimate_enrollment = models.PositiveSmallIntegerField()
     canvas_course_url = models.CharField(max_length=2000)
+
+    class Meta:
+        db_table = 'CouresOffering'
+        unique_together = ('term', 'course')
