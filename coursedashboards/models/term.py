44caccb4b31c02e9f96712788e1e43369bcfa4b2
Ethan Turner <ezturner@washington.edu>
2018-10-03 16:32:17 -0700

    Migrating to achieve parity with uw_sws

--- coursedashboards/models/term.py
+++ coursedashboards/models/term.py
@@ -34,35 +34,37 @@ class Term(models.Model):
 
         super(Term, self).save(*args, **kwargs)
 
-    def __cmp__(self, other):
-        return compare_terms(first, other)
+    def __eq__(self, other):
+        return (other is not None and
+                type(self) == type(other) and
+                self.int_key() == other.int_key())
 
-    @staticmethod
-    def compare_terms(first, other):
-        if first.year < other.year:
-            return -1
-        elif other.year < first.year:
-            return 1
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
-        if (Term._quarter_to_int(first.quarter) <
-                Term._quarter_to_int(other.quarter)):
-            return -1
+    def __lt__(self, other):
+        return (type(self) == type(other) and
+                self.int_key() < other.int_key())
 
-        return 1
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __gt__(self, other):
+        return (type(self) == type(other) and
+                self.int_key() > other.int_key())
+
+    def __ge__(self, other):
+        return self.__gt__(other) or self.__eq__(other)
 
     @staticmethod
     def _quarter_to_int(quarter):
         if quarter.lower() == Term.WINTER:
             return 1
-        elif quarter.lower() == Term.SPRING:
+        if quarter.lower() == Term.SPRING:
             return 2
-        elif quarter.lower() == Term.SUMMER:
+        if quarter.lower() == Term.SUMMER:
             return 3
-        elif quarter.lower() == Term.AUTUMN:
-            return 4
+        return 4
 
-    def __eq__(self, other):
-        return (other is not None and
-                type(self) == type(other) and
-                self.year == other.year and
-                self.quarter == other.quarter)
+    def int_key(self):
+        return int(self.year) * 10 + self._quarter_to_int(self.quarter)
