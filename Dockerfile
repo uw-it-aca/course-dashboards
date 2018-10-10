80bc41c1bed083549ced62b23d31975b5a142c96
Ethan Turner <ezturner@washington.edu>
2018-10-10 15:48:31 -0700

    Commiting work for Danny to get started

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:2.7
+WORKDIR /app/
+ENV PYTHONUNBUFFERED 1
+ADD coursedashboards/VERSION /app/coursedashboards/
+RUN apt-get update && apt-get install -qq python-dev libxml2-dev libxmlsec1-dev
+RUN pip install mysqlclient
+ADD setup.py /app/
+ADD requirements.txt /app/
+RUN pip install -r requirements.txt
+ADD . /app/
+ENV DB sqlite3
+RUN django-admin.py startproject project .
+ADD docker /app/project/
+ENV REMOTE_USER javerage
+CMD ["python", "manage.py", "runserver", "0:8000"]
---
+++ docker/settings.py
@@ -0,0 +1,166 @@
+"""
+Django settings for project project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+ALLOWED_HOSTS = ['*']
+
+
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'compressor',
+    'rc_django',
+    'templatetag_handlebars',
+    'coursedashboards',
+    'userservice',
+    'supporttools',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'userservice.user.UserServiceMiddleware',
+    'django_mobileesp.middleware.UserAgentDetectionMiddleware'
+)
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.RemoteUserBackend',
+
+)
+
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+ROOT_URLCONF = 'project.urls'
+
+WSGI_APPLICATION = 'project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+import os
+if os.environ['DB'] == "sqlite3":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+elif os.environ['DB'] == "mysql":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'myuw',
+            'USER': 'root',
+            'HOST': 'db',
+            'PASSWORD': 'docker',
+            'PORT': 3306,
+        }
+    }
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/Los_Angeles'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+COMPRESS_ENABLED = False
+COMPRESS_ROOT = "/static/"
+STATIC_ROOT = "/static/"
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug':  True,
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        }
+    }
+]
+
+# Thrive required settings
+
+MEDIA_ROOT = "/statics/hx_images"
+MEDIA_URL = "/uploaded_images/"
+
+
+#Support Tools settings
+SUPPORTTOOLS_PARENT_APP = "CoDa"
+SUPPORTTOOLS_PARENT_APP_URL = "/"
+
+REMOTE_USER_FORMAT = "uwnetid"
+
+MOCK_SAML_ATTRIBUTES = {
+    'uwnetid': ['javerage'],
+    'affiliations': ['student', 'member', 'alum', 'staff', 'employee'],
+    'eppn': ['javerage@washington.edu'],
+    'scopedAffiliations': ['student@washington.edu', 'member@washington.edu'],
+    'isMemberOf': ['u_test_group', 'u_test_another_group',
+                   'u_astratest_myuw_test-support-admin'],
+}
+
+from django.core.urlresolvers import reverse_lazy
+LOGIN_URL = reverse_lazy('saml_login')
+
+LOGOUT_URL = reverse_lazy('saml_logout')
