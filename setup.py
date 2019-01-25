214969b1fa9fc2bc2fcf996acbe9e8ce2bb07379
Ethan Turner <ezturner@washington.edu>
2019-01-24 17:45:39 -0800

    Adding sys to setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
+        'sys',
         'Django==1.11.10',
         'django-compressor',
         'django-pyscss',
