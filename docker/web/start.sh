468df56abfc9f1c0133bf8bd8f7ccb56309341db
Ethan Turner <ezturner@washington.edu>
2019-01-24 17:26:53 -0800

    Updating startup script to work with python 2

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -2,6 +2,8 @@
 # Remove any existing httpd data
 rm -rf /run/httpd/* /tmp/httpd*
 
+source "/app/bin/activate"
+
 export DATABASE_NAME=`echo $BRANCH | sed 's/-/_/g' `
 echo $DATABASE_NAME
 
@@ -10,12 +12,12 @@ then
   mysql -u $DATABASE_USERNAME -p$DATABASE_PASSWORD -h $DATABASE_HOSTNAME --execute="create database $DATABASE_NAME "
 fi
 
-python3 manage.py migrate
+python manage.py migrate
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 rm -rf /static/
-python3 manage.py collectstatic
+python manage.py collectstatic
 
 # Start Apache server in foreground
 exec /usr/sbin/apachectl -DFOREGROUND
