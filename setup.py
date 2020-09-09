fc38452d861d0d56d59a148072e34cadcc5bd174
mike seibel <mikes@washington.edu>
2020-09-09 06:49:19 -0700

    memcached sessions, vault

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'django-compressor==2.2',
         'django-pyscss',
         'django_mobileesp',
+        'python-memcached',
         'pytz',
         'AuthZ-Group>=1.6',
         'UW-RestClients-Core>=1.1.1,<2.0',
