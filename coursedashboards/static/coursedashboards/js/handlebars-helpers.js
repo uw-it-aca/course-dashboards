8466cfb78cf12ad735c93e8241d2ed73a0f10b2f
mike seibel <mikes@washington.edu>
2022-08-17 15:21:29 -0700

    jshint

--- coursedashboards/static/coursedashboards/js/handlebars-helpers.js
+++ coursedashboards/static/coursedashboards/js/handlebars-helpers.js
@@ -7,7 +7,7 @@ Handlebars.registerHelper('pluralize', function(number, single, plural) {
 
 Handlebars.registerHelper('roundPercentage', function(percentage) {
     if(percentage === 0){
-        return "0"
+        return "0";
     } else if(percentage < 1){
         return "<1";
     } else if (percentage > 100) {
