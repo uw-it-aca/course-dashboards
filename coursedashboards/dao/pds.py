eb14ab046e56e587bf7af60609c2c3821e17eebf
mike seibel <mikes@washington.edu>
2023-05-10 14:32:11 -0700

    tune pds mapping for profile data

--- coursedashboards/dao/pds.py
+++ coursedashboards/dao/pds.py
@@ -19,6 +19,19 @@ class CoDaLiveUWPersonClient(UWPersonClient):
     def get_persons_by_uwnetids(self, uwnetids, **kwargs):
         sqla_persons = self.DB.session.query(self.DB.Person).filter(
             self.DB.Person.uwnetid.in_(uwnetids))
+
+        kwargs.update({
+            'include_employee': False,
+            'include_student': True,
+            'include_student_transcripts': False,
+            'include_student_transfers': False,
+            'include_student_sports': False,
+            'include_student_advisers': False,
+            'include_student_majors': False,
+            'include_student_pending_majors': False,
+            'include_student_holds': False,
+            'include_student_degrees': False})
+
         return [self._map_person(p, **kwargs) for p in sqla_persons.all()]
 
     def get_persons_by_uwregids(self, uwregids, **kwargs):
