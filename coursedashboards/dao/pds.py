75df550ef66111a1162c3dba39ecede4480ae744
Jim Laney <jlaney@uw.edu>
2024-09-30 12:46:43 -0700

    add student pds query

--- coursedashboards/dao/pds.py
+++ coursedashboards/dao/pds.py
@@ -1,19 +1,23 @@
 # Copyright 2024 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-from uw_person_client.models import Person
-from uw_person_client.exceptions import PersonNotFoundException
+from django.db.models import CharField, OuterRef, Subquery
+from django.db.models.functions import JSONObject
+from uw_person_client.models import Student, Transcript
 
 
-def get_persons_by_uwnetids(uwnetids, **kwargs):
-    queryset = Person.objects.filter(uwnetid__in=uwnetids)
-
-    related_fields = Person.objects._include(**kwargs)
-    if related_fields:
-        queryset.prefetch_related(*related_fields)
-
-    persons = []
-    for person in queryset:
-        persons.append(Person.objects._assemble(person, **kwargs))
-
-    return persons
+def get_students_by_uwnetids(uwnetids):
+    return Student.objects.annotate(latest_transcript=Subquery(
+            Transcript.objects.filter(
+                student=OuterRef('pk')).values(json=JSONObject(
+                    scholarship_type='scholarship_type')
+                ).order_by('-tran_term__year', '-tran_term__quarter')[:1])
+        ).filter(
+            person__uwnetid__in=uwnetids
+        ).values(
+            'person__uwnetid',
+            'application_type_code',
+            'disability_ind',
+            'special_program_code',
+            'latest_transcript',
+        )
