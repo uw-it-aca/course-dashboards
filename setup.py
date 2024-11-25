bff7e12021f226c32b0f9be5f0b7600efa9a67fa
mike seibel <mikes@washington.edu>
2024-11-25 11:11:40 -0800

    import lti by hash

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'djangorestframework~=3.15',
         'Django-Persistent-Message',
         'django-person-client~=1.0',
-        'django-blti @ git+https://github.com/uw-it-aca/django-blti.git@feature/additive-1-point-3#egg=django-blti',
+        'django-blti @ git+https://github.com/uw-it-aca/django-blti.git@c0401ad6d2e1d5b802d6ce214f537a781ccca2da#egg=django-blti',
         'statistics',
     ],
     license='Apache License, Version 2.0',
