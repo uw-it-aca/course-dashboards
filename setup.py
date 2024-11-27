912e5b6ba0e20337ebc51dc1e6a3ba31b66aae3a
mike seibel <mikes@washington.edu>
2024-11-27 14:48:15 -0800

    bump deploy for lti

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'djangorestframework~=3.15',
         'Django-Persistent-Message',
         'django-person-client~=1.0',
-        'django-blti @ git+https://github.com/uw-it-aca/django-blti.git@9396077080c074179bd97e690ac8bd9de8407a6a#egg=django-blti',
+        'django-blti @ git+https://github.com/uw-it-aca/django-blti.git@9dd4609688a0e8c82e8f437e0174c24f47b5eacc#egg=django-blti',
         'statistics',
     ],
     license='Apache License, Version 2.0',
