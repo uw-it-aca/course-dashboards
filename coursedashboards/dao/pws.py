d20bf6ad58b166cce1676147ff76b01909cebcb0
mike seibel <mikes@washington.edu>
2026-05-04 13:36:54 -0700

    mock data shuffling

--- coursedashboards/dao/pws.py
+++ coursedashboards/dao/pws.py
@@ -22,6 +22,8 @@ def get_person_by_netid(netid):
     """
     Retrieve person data using the given netid
     """
+    if netid == "2DF6":
+        import pdb; pdb.set_trace()
     return PWS().get_person_by_netid(netid)
 
 
